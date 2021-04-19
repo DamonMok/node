23f0872420756ce39df8b474caf968506d8a9882
DamonMok <damonmok1216@gmail.com>
2021-04-19 18:22:44 +0800

    上传头像到服务器

--- src/router/fileRouter.js
+++ src/router/fileRouter.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router')
 const { verifyAuth } = require('../middleware/authMiddleware')
+const { avatarHandler } = require('../middleware/fileMiddleware')
 
 
-const fileRouter = new Router({ prefix: '/update' })
+const fileRouter = new Router({ prefix: '/upload' })
 
-fileRouter.post('/avatar', verifyAuth)
+fileRouter.post('/avatar', verifyAuth, avatarHandler)
 
 module.exports = fileRouter
